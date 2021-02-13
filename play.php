b50aeb7b83b695ec425db8e651a820ddcac9b95b
kawatatsutomu <>
2021-02-13 09:58:03 +0900

    Q8回答、動作確認OK

--- play.php
+++ play.php
@@ -1,6 +1,9 @@
 <?php
-$array = ["1", "2", "3", "4", "5"];
-
-  print_r(array_map('intval', $array));
-
+$programming_languages = ["php","ruby","python","javascript"];
+$programming_languages = array_map('ucfirst',$programming_languages);
+$upper_case_programming_languages = array_map('strtoupper',$programming_languages);
+print_r($programming_languages);
+echo PHP_EOL;
+print_r($upper_case_programming_languages);
+?>
 ?>
--- task.php
+++ task.php
@@ -108,12 +108,15 @@ print("#####q8#####".PHP_EOL);
 $programming_languages = ["php","ruby","python","javascript"];
 
   # 以下に回答を記載
-
+  <?php
+$programming_languages = ["php","ruby","python","javascript"];
+$programming_languages = array_map('ucfirst',$programming_languages);
+$upper_case_programming_languages = array_map('strtoupper',$programming_languages);
   # 以下は変更しないで下さい
 print_r($programming_languages);
 echo PHP_EOL;
 print_r($upper_case_programming_languages);
-
+?>
 echo PHP_EOL;
 
 print("#####q9#####".PHP_EOL);
