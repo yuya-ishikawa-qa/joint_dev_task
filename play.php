7c7d1f957a65265f950eb963d18b5e20be713fb1
kawatatsutomu <>
2021-02-27 18:55:04 +0900

    Q19回答しました。

--- play.php
+++ play.php
@@ -1,32 +1,19 @@
  <?php
+print("#####q19#####".PHP_EOL);
+class Item{
+  # 以下を修正して下さい
 
-print("#####q18#####".PHP_EOL);
+//   protected $name;
+    public $name;
 
-# コードを追加
-class Man
-{
-    protected $name;
-    protected $age;
-
-    function __construct($man_name, $man_age){
-        $this->name = $man_name;
-        $this->age = $man_age;
-    }
-    function introduce(){
-        if($this->age >= 20) {
-            print("こんにちは，".$this->name."と申します。宜しくお願いいたします。".PHP_EOL);
-        }else{
-            print("はいさいまいど〜，".$this->name."です！！！".PHP_EOL);
-        }
-    }
+  function __construct($book_name){
+    $this->name = $book_name;
+  }
 }
-
-$man1 = new Man("あじー",32);
-$man2 = new Man("ゆたぼん",10);
-
-$man1->introduce();
-$man2->introduce();
+  # 以下は変更しないで下さい
+// protectedではアクセスできない。変更してみたらできたけどなぜ？後回し
+$book = new Item("ゼロ秒思考");
+print($book->name.PHP_EOL);
 
 echo PHP_EOL;
-
 ?>
--- task.php
+++ task.php
@@ -346,22 +346,25 @@ echo PHP_EOL;
 
 ?>
 
+<?php
 print("#####q19#####".PHP_EOL);
 class Item{
   # 以下を修正して下さい
 
-  protected $name;
+//   protected $name;
+    public $name;
 
   function __construct($book_name){
     $this->name = $book_name;
   }
 }
   # 以下は変更しないで下さい
-
+// protectedではアクセスできない。変更してみたらできたけどなぜ？後回し
 $book = new Item("ゼロ秒思考");
 print($book->name.PHP_EOL);
 
 echo PHP_EOL;
+?>
 
 print("#####q20#####".PHP_EOL);
 class Human
